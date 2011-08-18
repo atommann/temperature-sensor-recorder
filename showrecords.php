252be9f493116690ac91b35101d2173e1a4e6ba1
Mitch Davis <mjd@afork.com>
2011-08-18 09:55:10 +0800

    Broke up the code into functions, and now we have the same buttons at the top and bottom

--- showrecords.php
+++ showrecords.php
@@ -1,5 +1,41 @@
 <?php
-    $refresh = 0;
+    function show_buttons($refresh)
+    {
+        $url = $_SERVER['REQUEST_URI'];
+
+        echo "<a href=\"{$url}\">[refresh]</a> ";
+        if ($refresh == 1) {
+            $url = preg_replace('/\?refresh$/', '', $url);
+            echo "<a href=\"{$url}\">[stop auto-refresh]</a> ";
+        } else {
+            echo "<a href=\"{$url}?refresh\">[start auto-refresh]</a>\n";
+        }
+        echo "<a href=/beertemp.php>[back]</a>";
+        echo "<br>\n";
+    }
+
+    function show_history()
+    {
+        # output a history of temperatures
+        echo "<pre>";
+        echo "Temperature History\n";
+        echo "Time                  Beer T  Ice T\n";
+
+        # open file "history.txt" in 'rb' mode
+        $fp = fopen("history.txt", 'rb');
+    
+        if ($fp) {
+            while (!feof($fp)) {
+                # read a line
+                $record = fgets($fp, 999);
+                echo $record;
+            }
+            # close file
+            fclose($fp);
+        }
+        echo "<br>\n";
+    }
+
     if (isset($_GET['refresh'])) {
        $refresh = 1;
     }
@@ -10,39 +46,15 @@
        echo "<meta http-equiv=\"refresh\" content=\"5\">\n";
     }
 
-    $url = $_SERVER['REQUEST_URI'];
     echo "</head>\n";
     echo "<body>\n";
-    echo "<a href=\"{$url}\">[refresh]</a> ";
-	if ($refresh == 1) {
-        $url = preg_replace('/\?refresh$/', '', $url);
-        echo "<a href=\"{$url}\">[stop auto-refresh]</a> ";
-	} else {
-        echo "<a href=\"{$url}?refresh\">[start auto-refresh]</a>\n";
-	}
-    echo "<a href=/beertemp.php>[back]</a>";
-    echo "<br>\n";
 
+    show_buttons($refresh);
+    show_history();
+    show_buttons($refresh);
 
-    # output a history of temperatures
-    echo "<pre>";
-    echo "Temperature History\n";
-    echo "Time                  Beer T  Ice T\n";
-
-    # open file "history.txt" in 'rb' mode
-    $fp = fopen("history.txt", 'rb');
-
-    if ($fp) {
-        while (!feof($fp)) {
-            # read a line
-            $record = fgets($fp, 999);
-            echo $record;
-        }
-        # close file
-        fclose($fp);
-    }
-
-    echo "</pre>";
-    echo "</body>";
-    echo "</html>";
+    echo "<a name=\"latest\">\n";
+    echo "</pre>\n";
+    echo "</body>\n";
+    echo "</html>\n";
 ?>
